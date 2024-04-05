6e93e4cbb111c574963a778dbf190b84a2bd08cb
hyerim03 <chhari0708@naver.com>
2024-04-05 11:24:09 +0900

    sec555555

---
+++ src/sec05/Hanoi.java
@@ -0,0 +1,16 @@
+package sec05;
+
+import java.util.Scanner;
+
+public class Hanoi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.println("하노이의 탑");
+		System.out.println("원반의 개수");
+
+	}
+
+}
--- src/sec05/Recur.java
+++ src/sec05/Recur.java
@@ -5,22 +5,20 @@ import java.util.Scanner;
 public class Recur {
 
 	static void recur(int n) {
-		if(y == 0)
-			return x;
-		else
-			return gcd(y, x%y);
+		if(n > 0) {
+			recur(n-1);
+			System.out.println(n);
+			recur(n-2);
+		}
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner stdIn = new Scanner(System.in);
-		
+
 		System.out.print("정수를 입력하세요.");
 		int x = stdIn.nextInt();
-		
-		int result = x * y / gcd(y, x%y);
-		
-		System.out.println("최소 공배수는 : " +result+"입니다");
+
 	}
 
 }
